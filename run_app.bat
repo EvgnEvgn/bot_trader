ebfa54b45563c689f376e8f878a470692e06b79d
Evgeniy Sharipov <sharipovevgn@gmail.com>
2019-03-24 14:25:52 +0300

    added bat to run app

--- run_app.bat
+++ run_app.bat
@@ -1,4 +1,3 @@
-::CALL %0\..\venv\Scripts\activate.bat
-::%PYTHON%\python.exe %0\..\trade_analyzer.py
-"C:\Python\python.exe" "C:\Users\User\PycharmProjects\bot_trader\trade_analyzer.py"
+%PYTHON%\python.exe %0\..\trade_analyzer.py
+::"C:\Python\python.exe" "C:\Users\User\PycharmProjects\bot_trader\trade_analyzer.py"
 pause
